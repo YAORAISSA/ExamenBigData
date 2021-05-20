db66f7c2a98efcff778674962e68fe51127d32c7
RCARINE <raissa.yao20@inphb.ci>
2021-05-20 12:24:33 +0000

    resultat de la fonction

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -2,7 +2,7 @@ object ExamenBigData {
 
   def main(args: Array[String]): Unit = {
 
-    val raissa = List("francklin","hiram", "ilan","karinesse")
+    val raissa = List("julien","paul", "jean","rac", "trec", "joel", "ed", "chris", "maurice")
 
     println(monexam(raissa))
 
@@ -13,5 +13,14 @@ object ExamenBigData {
     return k1
   }
 
+  def liste_double(): Unit = {
+
+  }
+
+  val montableau : Array[String][String] = Array("ecommercemag.fr","joel")
+  montableau(0) = "bety"
+
+  def main(args: Array[String]) {
+    var myMatrix = ofDim[Int](3,3)
 
 }
